5fb27df8e56f5a3a652c126a845d23b4c340fdce
anteon-ervin <97463962+anteon-ervin@users.noreply.github.com>
2022-01-10 12:12:10 -0500

    Delete csce204/assignments/assignment1 directory

--- csce204/assignments/assignment1/assignment1.py
+++
@@ -1,4 +0,0 @@
-# Author: Anteon Ervin
-print("My name is Anteon Ervin")
-print("An interesting fact about me is that I am a member of the US Air Force")
-print("My favorite thing to do is workout")
