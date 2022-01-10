d59ff813ae5088be60437104ab88dfd4c7bf033e
anteon-ervin <97463962+anteon-ervin@users.noreply.github.com>
2022-01-10 12:12:48 -0500

    Add files via upload

---
+++ assignments/assignment1/assignment1.py
@@ -0,0 +1,4 @@
+# Author: Anteon Ervin
+print("My name is Anteon Ervin")
+print("An interesting fact about me is that I am a member of the US Air Force")
+print("My favorite thing to do is workout")
