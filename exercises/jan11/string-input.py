f702b5b76cccb30b0a7d67ab9e9b91fbefeb682b
anteon-ervin <97463962+anteon-ervin@users.noreply.github.com>
2022-01-12 11:56:05 -0500

    Add files via upload

---
+++ exercises/jan11/string-input.py
@@ -0,0 +1,7 @@
+# Getting data from the user
+# Author: Anteon Ervin
+
+firstName = input("Enter first name: ")
+lastName = input("Enter last name: ")
+
+print(f"Welcome {firstName} {lastName}!")
---
+++ exercises/jan11/strings-vars.py
@@ -0,0 +1,14 @@
+# Learn strings and variables
+# Author: Anteon Ervin
+
+firstName = "Anteon"
+lastName = "Ervin"
+print("Welcome " + firstName + " " + lastName + "!")
+
+print(f"Welcome {firstName} {lastName}!")
+
+print("I love cheese\nBut I don't like milk.")
+
+print(f"""I love cheese
+But I don't 
+like milk""")
