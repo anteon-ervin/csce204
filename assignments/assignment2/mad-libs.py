5eb211569d499b84ab4aa26c20b330c9d3371696
anteon-ervin <97463962+anteon-ervin@users.noreply.github.com>
2022-01-12 12:28:57 -0500

    Add files via upload

---
+++ assignments/assignment2/mad-libs.py
@@ -0,0 +1,19 @@
+# Input, String
+# Author: Anteon Ervin
+
+firstNum = input("Number: ")
+secondNum = input("Another Number: ")
+animal = input("Animal: ")
+liquid = input("Liquid: ")
+thirdNum = input("Another Number: ")
+firstAdj = input("Adjective: ")
+sweetFood = input("Sweet Food: ")
+fourthNum = input("Another Number: ")
+secondAdj = input("Another Adjective: ")
+
+print("\n***** Mom's Cake Recipe *****")
+print(f"""
+First, preheat the oven to {firstNum} degrees. Then beat {secondNum} {animal} egg(s) with a cup of {liquid}.
+Next add {thirdNum} teaspoon(s) of baking soda and mix with eggs and {liquid} until it reaches a {firstAdj} texture.
+Don't forget the {sweetFood}! Pour into a 9 by 9 pan and bake for {fourthNum} minute(s) or until the cake is {secondAdj}.
+""")
