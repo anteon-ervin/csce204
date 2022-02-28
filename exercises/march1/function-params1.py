572ccda5bb7427af9d24e374468420448b14350f
anteon-ervin <97463962+anteon-ervin@users.noreply.github.com>
2022-02-28 13:59:12 -0500

    Add files via upload

---
+++ exercises/march1/function-params1.py
@@ -0,0 +1,16 @@
+# Function params
+# Author: Anteon Ervin
+
+def factorial(num):
+    answer = 1
+    
+    if num < 0:
+        print("Invalid number")
+        return
+    
+    for i in range(1, num + 1):
+        answer *= i 
+    
+    print(f"{num}! = {answer}")
+
+factorial(7)
---
+++ exercises/march1/functions1.py
@@ -0,0 +1,14 @@
+# Functions
+# Author: Anteon Ervin
+
+def say_hello():
+    print("Hello World")
+   
+def say_goodbye():
+    print("Goodbye")
+    
+say_hello()
+say_goodbye()
+say_hello()
+say_hello()
+        
